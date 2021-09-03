d1a2b13fd3d0201034a24036a9a8bbc02c86b43c
Morais Junior <geekdoagrest@gmail.com>
2021-09-03 16:51:23 -0300

    Set forcus for first input and optimize responsive layout

--- src/plugins/woocommerce-plug-payments/assets/js/transparent-checkout.js
+++ src/plugins/woocommerce-plug-payments/assets/js/transparent-checkout.js
@@ -9,9 +9,10 @@
 			$(this).parent().find('.plugpayments-method-form').hide();
 			const key = $(this).find('input').val();
 			$('#plugpayments-'+key+'-form').show();
+			if( $('#plugpayments-'+key+'-form input').length >  0){
+				$('#plugpayments-'+key+'-form input')[0].focus();
+			}
 		});
-
-
 	});
 
 }( jQuery ));
