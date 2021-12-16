d301dcb7e7cda51567feaf34be2de2cc29937731
Lucas Coutinho <lrclucas@gmail.com>
2021-12-16 10:09:21 -0300

    fix browser copy issues

--- src/plugins/woocommerce-plug-payments/templates/receipt/pix.php
+++ src/plugins/woocommerce-plug-payments/templates/receipt/pix.php
@@ -41,12 +41,66 @@
 </div>
 
 <script>
+
     var copyButton = document.querySelector("#plug_copy_pix");
-    function copy() {
+
+    function wrapperToCopy() {
         const cb = navigator.clipboard;
-        var copyText = document.querySelector("#plug_qr_data").innerText;
-        cb.writeText(copyText.value).then(() => copyButton.innerHTML = "Copiado!");
+        var copyText = document.querySelector("#plug_qr_data").textContent;
+        if (copyToClipboard(copyText)) {
+            copyButton.textContent = "Copiado!";
+            setTimeout(() => {
+                copyButton.textContent = "Copiar";
+            }, 2000);
+        } else {
+            alert("Não foi possível copiar o código QR!");
+        }
 
     }
-    copyButton.addEventListener("click", copy);
+    copyButton.addEventListener("click", wrapperToCopy);
+
+    function copyToClipboard(string) {
+        let textarea;
+        let result;
+
+        try {
+            textarea = document.createElement('textarea');
+            textarea.setAttribute('readonly', true);
+            textarea.setAttribute('contenteditable', true);
+            textarea.style.position = 'fixed'; // prevent scroll from jumping to the bottom when focus is set.
+            textarea.value = string;
+
+            document.body.appendChild(textarea);
+
+            textarea.focus();
+            textarea.select();
+
+            const range = document.createRange();
+            range.selectNodeContents(textarea);
+
+            const sel = window.getSelection();
+            sel.removeAllRanges();
+            sel.addRange(range);
+
+            textarea.setSelectionRange(0, textarea.value.length);
+            result = document.execCommand('copy');
+        } catch (err) {
+            console.error(err);
+            result = null;
+        } finally {
+            document.body.removeChild(textarea);
+        }
+
+        // manual copy fallback using prompt
+        if (!result) {
+            const isMac = navigator.platform.toUpperCase().indexOf('MAC') >= 0;
+            const copyHotkey = isMac ? '⌘C' : 'CTRL+C';
+            result = prompt(`Press ${copyHotkey}`, string); // eslint-disable-line no-alert
+            if (!result) {
+                return false;
+            }
+        }
+        return true;
+    }
+ 
 </script>
