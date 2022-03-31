7b0a1f4af093cbb238f8a23b061b216f733d656f
Morais Junior <geekdoagrest@gmail.com>
2022-03-31 12:37:39 -0300

    Add discount of paymenttype

--- src/plugins/woocommerce-plug-payments/includes/poedit.php
+++ src/plugins/woocommerce-plug-payments/includes/poedit.php
@@ -5,3 +5,6 @@ _e( 'Invalid CVV', 'plug-payments-gateway' );
 _e( 'Enable Credit', 'plug-payments-gateway' );
 _e( 'Enable Pix', 'plug-payments-gateway' );
 _e( 'Enable Boleto', 'plug-payments-gateway' );
+_e( 'credit discount', 'plug-payments-gateway' );
+_e( 'pix discount', 'plug-payments-gateway' );
+_e( 'boleto discount', 'plug-payments-gateway' );
